73d2d739b14171e0f7be813bbe2c11e87216c42c
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-13 09:36:59 +0200

    Add Kartya class

---
+++ src/hu/petrik/bank/Kartya.java
@@ -0,0 +1,25 @@
+package hu.petrik.bank;
+
+public class Kartya extends BankiSzolgaltatas{
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg){
+        if(this.szamla.getAktualisEgyenleg()-osszeg<0){
+            return false;
+        }else{
+            this.szamla.kivesz(osszeg);
+            return true;
+        }
+    }
+}
