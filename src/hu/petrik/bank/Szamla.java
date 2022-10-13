e7e1bab553594ae10a1d7b8b70fa1c7a93f82f5e
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-13 08:33:58 +0200

    Add Szamla class

---
+++ src/hu/petrik/bank/Szamla.java
@@ -0,0 +1,29 @@
+package hu.petrik.bank;
+
+public class Szamla extends BankiSzolgaltatas {
+
+    private int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.aktualisEgyenleg = 0;
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg){
+        this.aktualisEgyenleg+=osszeg;
+    }
+
+    public boolean kivesz(int osszeg){
+        if(this.aktualisEgyenleg-osszeg<=0){
+            this.aktualisEgyenleg=0;
+            return false;
+        }else{
+            this.aktualisEgyenleg-=osszeg;
+            return true;
+        }
+    }
+}
