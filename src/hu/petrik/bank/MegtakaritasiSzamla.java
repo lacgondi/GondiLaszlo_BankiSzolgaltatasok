a113359e56dddcc92199867936c1eab3e2aeb37d
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-13 09:13:50 +0200

    Add MegtakaritasiSzamla class

---
+++ src/hu/petrik/bank/MegtakaritasiSzamla.java
@@ -0,0 +1,28 @@
+package hu.petrik.bank;
+
+public class MegtakaritasiSzamla extends Szamla{
+    private double kamat;
+    private static double alapKamat = 2;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.kamat = alapKamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        return super.kivesz(osszeg);
+    }
+
+    public void kamatJovairas(){
+        befizet((int) (this.getAktualisEgyenleg()*(kamat/100)));
+    }
+}
