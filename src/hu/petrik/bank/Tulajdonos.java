f50138c27386d05c1c812133a828a511106c6006
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-13 08:19:07 +0200

    Make Tulajdonos class

--- src/hu/petrik/bank/Tulajdonos.java
+++ src/hu/petrik/bank/Tulajdonos.java
@@ -1,4 +1,17 @@
 package hu.petrik.bank;
 
-public class Tulajdonos {
+public final class Tulajdonos {
+    private String name;
+
+    public Tulajdonos(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
